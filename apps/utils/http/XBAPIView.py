f0269478024253b3375c1def39fc1c531c61d70a
独头茧 <18795207631@163.com>
2020-01-12 19:47:55 +0800

    更新基础函数处理库

--- apps/utils/http/XBAPIView.py
+++ apps/utils/http/XBAPIView.py
@@ -14,27 +14,9 @@ from utils.http.XBHTTPCode import error_msg
 
 class XBListModelMixin(viewsets.GenericViewSet,
                        mixins.ListModelMixin):
-    """
-    list:
-        产品
-    """
-    html = ""
-
-    def set_html(self, html):
-        self.html = html
-
-    def get_renderers(self):
-        """
-        Instantiates and returns the list of renderers that this view can use.
-        """
-        a = self.request.META.get("HTTP_A", 0)
-
-        if a == "1":
-            return [renderer() for renderer in [TemplateHTMLRenderer]]
-        else:
-            return [renderer() for renderer in self.renderer_classes]
 
     def list(self, request, *args, **kwargs):
+        self.check_object_permissions(self.request, 'a')
         method = self.request.META["REQUEST_METHOD"].lower()
         try:
             queryset = self.filter_queryset(self.get_queryset())
@@ -43,45 +25,10 @@ class XBListModelMixin(viewsets.GenericViewSet,
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            # return CodeStatus(type=method,data=serializer.data)
-            return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(queryset, many=True)
-        if self.html:
-            return CodeStatus(type=method, data=serializer.data, html=self.html)
-        else:
-            return CodeStatus(type=method, data=serializer.data)
-
-
-class XBHtmlListModelMixin(viewsets.GenericViewSet,
-                       mixins.ListModelMixin):
-    """
-    列表处理
-    """
-    html = ""
-
-    def set_html(self, html):
-        self.html = html
-
-    def get_renderers(self):
-        """
-        Instantiates and returns the list of renderers that this view can use.
-        """
-        # a = self.request.META.get("HTTP_A", 0)
-
-        return [renderer() for renderer in [TemplateHTMLRenderer]]
-
-
-    def list(self, request, *args, **kwargs):
-        method = self.request.META["REQUEST_METHOD"].lower()
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(queryset, many=True)
 
-        return CodeStatus(type=method, data=serializer.data, html=self.html)
-
+        return CodeStatus(type=method, data=serializer.data)
 
 
 class XBRetrieveModelMixin(viewsets.GenericViewSet,
@@ -93,17 +40,16 @@ class XBRetrieveModelMixin(viewsets.GenericViewSet,
             详情处理
         """
         method = self.request.META["REQUEST_METHOD"].lower()
-        # try:
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return CodeStatus(type=method, data=serializer.data)
-
-        # except:
-        #     return Response(status=status.HTTP_404_NOT_FOUND, data={
-        #         "status": {
-        #             "code": ResponseSatatusCode.HTTPCODE_40004_NOT_FIND.value,
-        #             "msg": "无效页面"
-        #         }})
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            return CodeStatus(type=method, data=serializer.data)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={
+                "status": {
+                    "code": ResponseSatatusCode.HTTPCODE_40004_NOT_FIND.value,
+                    "msg": "无效页面"
+                }})
 
 
 class XBCreateModelMixin(viewsets.GenericViewSet,
@@ -123,33 +69,6 @@ class XBCreateModelMixin(viewsets.GenericViewSet,
         except:
             headers = self.get_success_headers(serializer.data)
             return Response(error_msg(serializer._errors), status=status.HTTP_400_BAD_REQUEST, headers=headers)
-
-
-
-class XbHTMLCreateModelMixin(viewsets.GenericViewSet,
-                         mixins.CreateModelMixin):
-    """
-    创建资源
-    """
-    html = ""
-
-    def set_html(self, html):
-        self.html = html
-
-    def get_renderers(self):
-        return [renderer() for renderer in [TemplateHTMLRenderer]]
-
-    def create(self, request, *args, **kwargs):
-        method = self.request.META["REQUEST_METHOD"].lower()
-        serializer = self.get_serializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except:
-            headers = self.get_success_headers(serializer.data)
-            return Response(error_msg(serializer._errors), status=status.HTTP_400_BAD_REQUEST, headers=headers)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return CodeStatus(type=method, data=serializer.data, html=self.html, header=headers)
 
 
 class XBDestroyModelMixin(viewsets.GenericViewSet,
