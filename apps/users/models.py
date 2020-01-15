35545c6935cb79547e3b3171c018e60b4f1d0a5c
独头茧 <18795207631@163.com>
2020-01-15 14:29:01 +0800

    修改用户地址实体

--- apps/users/models.py
+++ apps/users/models.py
@@ -32,12 +32,12 @@ class UserAddress(models.Model):
     用户收货地址
     """
     user = models.ForeignKey(UserProfile, verbose_name="用户" ,on_delete=models.CASCADE)
-    province = models.CharField(max_length=100, default="", verbose_name="省份",help_text="省份")
-    city = models.CharField(max_length=100, default="", verbose_name="城市",help_text="城市")
-    district = models.CharField(max_length=100, default="", verbose_name="区域",help_text="区域")
-    address = models.CharField(max_length=100, default="", verbose_name="详细地址",help_text="详细地址")
-    signer_name = models.CharField(max_length=100, default="", verbose_name="签收人",help_text="签收人")
-    signer_mobile = models.CharField(max_length=11, default="", verbose_name="电话",help_text="电话")
+    province = models.CharField(max_length=100, verbose_name="省份",help_text="省份")
+    city = models.CharField(max_length=100, verbose_name="城市",help_text="城市")
+    district = models.CharField(max_length=100, verbose_name="区域",help_text="区域")
+    address = models.CharField(max_length=100, verbose_name="详细地址",help_text="详细地址")
+    signer_name = models.CharField(max_length=100, verbose_name="签收人",help_text="签收人")
+    signer_mobile = models.CharField(max_length=11, verbose_name="电话",help_text="电话")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
