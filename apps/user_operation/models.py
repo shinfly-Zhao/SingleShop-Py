32b26c7ad27a26355123f61578661a4719c219e6
独头茧 <18795207631@163.com>
2020-01-23 14:11:10 +0800

    修改用户优惠券是否可用状态

--- apps/user_operation/models.py
+++ apps/user_operation/models.py
@@ -8,6 +8,7 @@ class UserCoupons(models.Model):
     user = models.ForeignKey(UserProfile, verbose_name="所属用户",help_text="所属用户",on_delete=models.CASCADE)
     coupon = models.ForeignKey(ShopCoupons, verbose_name="购物券",help_text="购物券",on_delete=models.CASCADE)
     add_time = models.DateTimeField(default=datetime.now, verbose_name="发布时间")
+    is_use = models.BooleanField(default=True, verbose_name="是否可用", help_text="是否可用")
 
     class Meta:
         verbose_name = "用户-购物券"
